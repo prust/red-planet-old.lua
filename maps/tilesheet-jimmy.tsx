78b937e2ec9cf280ff94efb3a2be4408ac341a9c
Jimmy Rust <rustjam000@bellinghamschools.org>
2020-11-20 07:20:05 -0800

    Added Jimmy's graphics

---
+++ maps/tilesheet-jimmy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="for dad's game" tilewidth="160" tileheight="160" tilecount="110" columns="10">
+ <image source="../images/tilesheet-jimmy.png" width="1600" height="1760"/>
+</tileset>
